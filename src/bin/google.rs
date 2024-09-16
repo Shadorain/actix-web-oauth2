362bf00e36d2547674db7de101ade4186c862781
Shadorain <jordanf7517@gmail.com>
2024-09-16 09:59:57 -0400

    feat: Initial project. Google done (without login check)

---
+++ src/bin/google.rs
@@ -0,0 +1,135 @@
+use std::env;
+
+use actix_web::{get, http::header, web, App, HttpResponse, HttpServer};
+use oauth2::{
+    basic::BasicClient, AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken,
+    PkceCodeChallenge, RedirectUrl, Scope, TokenUrl,
+};
+use serde::Deserialize;
+
+struct AppState {
+    oauth: BasicClient,
+}
+
+#[derive(Deserialize)]
+pub struct AuthRequest {
+    code: String,
+    state: String,
+    scope: String,
+}
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    let link = "login";
+
+    let html = format!(
+        r#"<html>
+        <head><title>OAuth2 Test</title></head>
+        <body>
+            <a href="/{}">{}</a>
+        </body>
+    </html>"#,
+        link, link
+    );
+    HttpResponse::Ok().body(html)
+}
+
+#[get("/login")]
+async fn login(data: web::Data<AppState>) -> HttpResponse {
+    // Google supports Proof Key for Code Exchange (PKCE - https://oauth.net/2/pkce/).
+    // Create a PKCE code verifier and SHA-256 encode it as a code challenge.
+    let (pkce_code_challenge, _pkce_code_verifier) = PkceCodeChallenge::new_random_sha256();
+
+    // Generate the authorization URL to which we'll redirect the user.
+    let (authorize_url, _csrf_state) = &data
+        .oauth
+        .authorize_url(CsrfToken::new_random)
+        // This example is requesting access to the "calendar" features and the user's profile.
+        .add_scope(Scope::new(
+            "https://www.googleapis.com/auth/calendar".to_string(),
+        ))
+        .add_scope(Scope::new(
+            "https://www.googleapis.com/auth/plus.me".to_string(),
+        ))
+        .set_pkce_challenge(pkce_code_challenge)
+        .url();
+
+    HttpResponse::Found()
+        .append_header((header::LOCATION, authorize_url.to_string()))
+        .finish()
+}
+
+#[get("/logout")]
+async fn logout() -> HttpResponse {
+    HttpResponse::Found()
+        .append_header((header::LOCATION, "/"))
+        .finish()
+}
+
+#[get("/auth")]
+async fn auth(data: web::Data<AppState>, params: web::Query<AuthRequest>) -> HttpResponse {
+    let code = AuthorizationCode::new(params.code.clone());
+    let state = CsrfToken::new(params.state.clone());
+    let _scope = params.scope.clone();
+
+    // Exchange the code with a token.
+    let token = &data.oauth.exchange_code(code);
+
+    let html = format!(
+        r#"<html>
+        <head><title>OAuth2 Test</title></head>
+        <body>
+            Google returned the following state:
+            <pre>{}</pre>
+            Google returned the following token:
+            <pre>{:?}</pre>
+        </body>
+    </html>"#,
+        state.secret(),
+        token
+    );
+    HttpResponse::Ok().body(html)
+}
+
+#[actix_web::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    HttpServer::new(move || {
+        let google_client_id = ClientId::new(
+            env::var("GOOGLE_CLIENT_ID")
+                .expect("Missing the GOOGLE_CLIENT_ID environment variable."),
+        );
+        let google_client_secret = ClientSecret::new(
+            env::var("GOOGLE_CLIENT_SECRET")
+                .expect("Missing the GOOGLE_CLIENT_SECRET environment variable."),
+        );
+        let auth_url = AuthUrl::new("https://accounts.google.com/o/oauth2/v2/auth".to_string())
+            .expect("Invalid authorization endpoint URL");
+        let token_url = TokenUrl::new("https://www.googleapis.com/oauth2/v3/token".to_string())
+            .expect("Invalid token endpoint URL");
+
+        // Set up the config for the Google OAuth2 process.
+        let oauth = BasicClient::new(
+            google_client_id,
+            Some(google_client_secret),
+            auth_url,
+            Some(token_url),
+        )
+        .set_redirect_uri(
+            RedirectUrl::new("http://127.0.0.1:5000/auth".to_string())
+                .expect("Invalid redirect URL"),
+        );
+        App::new()
+            .app_data(web::Data::new(AppState { oauth }))
+            .service(index)
+            .service(login)
+            .service(logout)
+            .service(auth)
+    })
+    .bind(("127.0.0.1", 5000))
+    .unwrap()
+    .run()
+    .await
+    .unwrap();
+}
